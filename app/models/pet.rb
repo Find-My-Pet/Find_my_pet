8206b16dbd753685d89b7c206b9111790bb011ce
JohnnyZhou <yza90127@gmail.com>
2016-11-19 11:46:21 -0800

    pet model validates

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :user
+  validates :pet_type, presence: true
+  validates :breed, presence: true
+  validates :color, presence: true
+  validates :gender, presence: true
+  validates :last_seen_at, presence: true
 end
