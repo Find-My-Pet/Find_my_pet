aa64a14f5931333248104eb91a93c86077d89873
octothorpus <octothorpus@gmail.com>
2016-11-19 14:41:07 -0800

    preparing to recieve navbar

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20161119172820) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.boolean  "found"
     t.float    "lat"
     t.float    "long"
     t.index ["user_id"], name: "index_pets_on_user_id", using: :btree
