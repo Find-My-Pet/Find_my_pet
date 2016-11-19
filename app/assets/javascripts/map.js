8cdd69968aafea8b63edc0ba61872a516f647644
Paul WB <higgsbison@gmail.com>
2016-11-19 15:48:00 -0800

    added search and geolocate to mapp

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,7 +2,7 @@ const info =
 `<div id = content>
   <div id = "photo" style="float: left">
     <img src="https://www.dogstrust.org.uk/sponsor/_dogs/digby-assets/v800_digby01.jpg" alt="dog" height="160" width="120">
-  </div>            
+  </div>
   <div id="pet-info" style="float: right; padding: 1em;">
     <h2>Dog Name</h2>
     <h5>Type</h5>
@@ -27,17 +27,96 @@ $( document ).ready(function() {
 });
 
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+
   map = new google.maps.Map(document.getElementById('map'), {
-   zoom: 4,
-   center: uluru
+    center: {lat: 49.28200018372185, lng: -123.10863018035889},
+    zoom: 14,
+    mapTypeId: 'roadmap',
+    streetViewControl: false,
   });
-  
+
   google.maps.event.addListener(map, 'click', function(event) {
     closeAllInfoWindows();
     addMarker(event.latLng, map);
   });
-  
+
+  var infoWindow = new google.maps.InfoWindow({map: map});
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  var geoloccontrol = new klokantech.GeolocationControl(map, 15);
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  searchBox.addListener('places_changed', function() {
+         var places = searchBox.getPlaces();
+
+         if (places.length == 0) {
+           return;
+         }
+
+         // Clear out the old markers.
+         markers.forEach(function(marker) {
+           marker.setMap(null);
+         });
+         markers = [];
+
+         // For each place, get the icon, name and location.
+         var bounds = new google.maps.LatLngBounds();
+         places.forEach(function(place) {
+           if (!place.geometry) {
+             console.log("Returned place contains no geometry");
+             return;
+           }
+           var icon = {
+             url: place.icon,
+             size: new google.maps.Size(71, 71),
+             origin: new google.maps.Point(0, 0),
+             anchor: new google.maps.Point(17, 34),
+             scaledSize: new google.maps.Size(25, 25)
+           };
+
+           // Create a marker for each place.
+           markers.push(new google.maps.Marker({
+             map: map,
+             icon: icon,
+             title: place.name,
+             position: place.geometry.location
+           }));
+
+           if (place.geometry.viewport) {
+             // Only geocodes have viewport.
+             bounds.union(place.geometry.viewport);
+           } else {
+             bounds.extend(place.geometry.location);
+           }
+         });
+         map.fitBounds(bounds);
+       });
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+
   return map;
 }
 
@@ -63,20 +142,20 @@ var icons = {
   }
 };
 
-  // Creae a infor window on the map with given coords       
+  // Creae a infor window on the map with given coords
   var infowindow = new google.maps.InfoWindow({
     content: info,
     position: coords
   });
   // add info window to the inforWindows array
-	infoWindows.push(infowindow); 
+	infoWindows.push(infowindow);
   // Leave a marker at the same location
   var marker = new google.maps.Marker({
     position: coords,
     map: map,
     icon: icons[type].icon
   });
-  
+
   // Click on marker will pop up info
   marker.addListener('click', function() {
     closeAllInfoWindows();
@@ -93,7 +172,7 @@ function addMarker(location, map, label) {
     map: map
   });
   markers.push(marker);
-// markers[markers.length - 1] is the location we want to submit on the form        
+// markers[markers.length - 1] is the location we want to submit on the form
 // console.log(markers[markers.length - 1]);
   if (markers.length > 1) {
     removeLastMarker()
