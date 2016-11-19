64425ad087562ca38b5162c8d52ab5346aebe1fe
Thaisa Marinho <thaisa_marinho@hotmail.com>
2016-11-18 20:08:24 -0800

    create project and models

---
+++ db/schema.rb
@@ -0,0 +1,73 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20161119035633) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "pet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pet_id"], name: "index_messages_on_pet_id", using: :btree
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.string   "type"
+    t.string   "name"
+    t.string   "breed"
+    t.string   "color"
+    t.string   "gender"
+    t.string   "age"
+    t.string   "last_seen_at"
+    t.string   "lat"
+    t.string   "long"
+    t.text     "note"
+    t.string   "image"
+    t.datetime "date_time"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_pets_on_user_id", using: :btree
+  end
+
+  create_table "sightings", force: :cascade do |t|
+    t.string   "type"
+    t.string   "last_seen_at"
+    t.string   "long"
+    t.string   "lat"
+    t.datetime "date_time"
+    t.text     "note"
+    t.string   "image"
+    t.string   "name"
+    t.string   "contact"
+    t.integer  "pet_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["pet_id"], name: "index_sightings_on_pet_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_foreign_key "messages", "pets"
+  add_foreign_key "pets", "users"
+  add_foreign_key "sightings", "pets"
+end
